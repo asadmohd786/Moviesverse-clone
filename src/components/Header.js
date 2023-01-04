0c7271cff4bf151691794b5339bf1d8f0ab75253
asadmohd786 <92883306+asadmohd786@users.noreply.github.com>
2023-01-04 17:49:57 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,72 +1,69 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-
-
-
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
+  '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
     marginLeft: theme.spacing(1),
-    width: "auto",
+    width: 'auto',
   },
 }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
+const SearchIconWrapper = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
+  color: 'inherit',
+  '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "19ch",
-      "&:focus": {
-        width: "29ch",
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '30ch',
+      '&:focus': {
+        width: '45ch',
       },
     },
   },
 }));
 
-const Header = (_) => {
+const Header = _ => {
+
   const navigate = useNavigate();
 
-  const keyPressed = (e) => {
-    if (e.code === "Enter") {
+  const keyPressed = e => {
+    if(e.code === "Enter") {
       const query = e.target.value;
       navigate(`/search?q=${query}`);
       // console.log(query);
     }
-  };
- 
-  
+  }
+
+  return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
@@ -74,37 +71,26 @@ const Header = (_) => {
             variant="h5"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            <Link
-              to="/"
-              style={{
-                textDecoration: "none",
-               
-                color: "black",
-          
-              }}
-            >
+            <Link to="/" style={{textDecoration: "none", color: "white"}}>
               Moviesverse
             </Link>
           </Typography>
-        
-          
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-              placeholder="Movies,Series,Games,Bio,etc..."
-              inputProps={{ "aria-label": "search" }}
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
               onKeyUp={keyPressed}
             />
           </Search>
         </Toolbar>
       </AppBar>
-     
     </Box>
-  
-};
+  )
+}
 
 export default Header;
